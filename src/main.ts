8b3ac485b47ba9ebcd4d238d4165ffc5b2d6516f
ChungusBeliever <ChungusBeliever@users.noreply.github.com>
2026-02-14 02:57:47 -0800

    Added functionality to click to add new buttons

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,18 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+const addButton = document.querySelector<HTMLButtonElement>('#add-sticker-btn');
+const stickerContainer = document.querySelector<HTMLDivElement>('#sticker-container');
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+function addSticker() {
+    if (stickerContainer == null) {
+        return;
+    }
+    const newButton = document.createElement('button');
+    newButton.className = 'sticker';
+    newButton.innerText = 'hello';
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+    stickerContainer.appendChild(newButton);
+    stickerContainer.scrollTop = stickerContainer.scrollHeight;
+}
+
+addButton?.addEventListener('click', () => {
+    addSticker();
+})
