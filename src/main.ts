e17c1f46bd0bf3de1aa640ba169c8a2f35e44da1
ChungusBeliever <ChungusBeliever@users.noreply.github.com>
2026-02-14 16:19:28 -0800

    added the no sticker message

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,15 @@ const addButton = document.querySelector<HTMLButtonElement>('#add-sticker-btn');
 const stickerContainer = document.querySelector<HTMLDivElement>('#sticker-container');
 const stickerList: Sticker[] = [];
 
+// Adds a new sticker to the list of stickers and the div element.
+// Also removes the no sticker message if its there
 function addSticker() {
     if (stickerContainer == null) {
         return;
     }
+
+    removeNoStickerMessage(stickerContainer);
+    
     const newButton = new Sticker;
     stickerList.push(newButton);
 
@@ -15,6 +20,31 @@ function addSticker() {
     stickerContainer.scrollTop = stickerContainer.scrollHeight;
 }
 
+// helper function create the no sticker message
+function createNoStickerMessage() {
+    if (stickerList.length == 0) {
+        const noStickerMessage: HTMLSpanElement = document.createElement('span');
+        noStickerMessage.innerText = 'No stickers available. Add one';
+        noStickerMessage.id = 'no-sticker-message';
+        stickerContainer?.appendChild(noStickerMessage);
+    }
+}
+
+// helper function remove the no sticker message
+function removeNoStickerMessage(stickerContainer: HTMLDivElement) {
+    const noStickerMessageMessage = document.querySelector<HTMLSpanElement>('#no-sticker-message');
+    if (noStickerMessageMessage != null) {
+        stickerContainer?.removeChild(noStickerMessageMessage);
+    }
+}
+
+
+// loads in the stickers. Creates a no sticker message if theres no stickers to be loaded
+function loadStickers() {
+    createNoStickerMessage();
+}
+
 addButton?.addEventListener('click', () => {
     addSticker();
 })
+loadStickers();
